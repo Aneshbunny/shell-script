ad6ae4be6f8642bdb038b3c26905f2b3db0a7f87
Aneshbunny <iamaneshathkuri@gmail.com>
2026-02-12 21:35:14 +0530

    Names are added

---
+++ 03-conversions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Anesh:: hello Suresh,how are you?"
+echo "Suresh:: Hai Anesh,Iam good.what about you?"
+echo "Anesh:: Iam over the moon.what are you doing?"
+echo "Suresh:: Iam learning DevSecOps this month."
