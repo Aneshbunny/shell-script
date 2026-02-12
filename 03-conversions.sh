73835422811acb0565e006a0b72facdee046ff96
Aneshbunny <iamaneshathkuri@gmail.com>
2026-02-12 21:47:33 +0530

    names are changed

--- 03-conversions.sh
+++ 03-conversions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 PERSON1=Praveen
 PERSON2=Rahul
+PERSON3=Suresh
 echo "$PERSON1:: hello $PERSON2,how are you?"
 echo "$PERSON2:: Hai $PERSON1,Iam good.what about you?"
 echo "$PERSON1:: Iam over the moon.what are you doing?"
 echo "$PERSON2:: Iam learning DevSecOps this month."
+echo "$PERSON3:: Hai Suresh."
