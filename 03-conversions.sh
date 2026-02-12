62822d711a3a9434c7163a562e19d613cb96694d
Aneshbunny <iamaneshathkuri@gmail.com>
2026-02-12 21:43:27 +0530

    some changes

--- 03-conversions.sh
+++ 03-conversions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Anesh:: hello Suresh,how are you?"
-echo "Suresh:: Hai Anesh,Iam good.what about you?"
-echo "Anesh:: Iam over the moon.what are you doing?"
-echo "Suresh:: Iam learning DevSecOps this month."
+PERSON1=Anesh
+PERSON2=Suresh
+echo "$PERSON1:: hello $PERSON2,how are you?"
+echo "$PERSON2:: Hai $PERSON1,Iam good.what about you?"
+echo "$PERSON1:: Iam over the moon.what are you doing?"
+echo "$PERSON2:: Iam learning DevSecOps this month."
