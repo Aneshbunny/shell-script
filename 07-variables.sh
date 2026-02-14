c5b8efceaf26dca517c46c1ee94ecf5ca8b95489
Aneshbunny <iamaneshathkuri@gmail.com>
2026-02-14 15:17:07 +0530

    names are changed

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-START_TIME=$(date)
+START_TIME=$(date+%s)
 echo "Script excueted at:$START_TIME"
+sleep 50
+END_TIME=$(date+%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Script exceuted in:$TOTAL_TIME seconds"
